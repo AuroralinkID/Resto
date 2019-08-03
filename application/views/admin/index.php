2d8b25a9beee8be17b96c414e6e4ce02dba2c035
root <auroralinkid@gmail.com>
2019-08-03 23:21:29 +0700

    first commit

---
+++ application/views/admin/index.php
@@ -0,0 +1,29 @@
+
+<!-- AWAL MAIN-->
+<main class="main">
+        <!-- Breadcrumb-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">Home</li>
+          <li class="breadcrumb-item">
+            <a href="#">Admin</a>
+          </li>
+          <li class="breadcrumb-item active">Dashboard</li>
+          <!-- Breadcrumb Menu-->
+          <li class="breadcrumb-menu d-md-down-none">
+            <div class="btn-group" role="group" aria-label="Button group">
+              <a class="btn" href="#">
+                <i class="icon-speech"></i>
+              </a>
+            </div>
+          </li>
+        </ol>
+        <div class="container-fluid">
+          <div class="animated fadeIn">
+                <!—- Awal Koding-->
+
+     		   <!-- Akhir Koding -->
+          </div>
+        </div>
+      </main>
+	  <!-- AKHIR MAIN-->
+
---
+++ application/views/tema/footer.php
@@ -0,0 +1,23 @@
+<!-- AWALAN FOOTER-->
+<aside class="aside-menu">
+    </div>
+    </aside>
+    <footer class="app-footer">
+      <div>
+        <a href="#">UKK</a>
+        <span>© 2019 .</span>
+      </div>
+      <div class="ml-auto">
+        <span>DI</span>
+        <a href="#">SMK NURUL JADID</a>
+      </div>
+    </footer>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/pace-progress/pace.min.js"></script>
+    <script src="node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+    <script src="node_modules/@coreui/coreui/dist/js/coreui.min.js"></script>
+  </body>
+</html>
+<!-- AKHIR FOOTER-->
